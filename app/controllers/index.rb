88338a4bc7c33c83f1790672c9fa808da2445ed0
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 00:09:21 -0500

    working on params and passing to model blog and tags

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,21 @@
+before do
+  @posts = []
+end
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+get '/post/create' do
+  puts #{params}
+  erb :post_create
+end
+
+post '/post/create' do
+  @args = {title: params[":blog"][":title"], body: params[":blog"][":body"]}
+  Blog.create{@args}
+  @tags = {tags: params["blog"][":tags"]}
+  #Blog.tags << @tags.map {|tag| Tag.find_or_create_by_name(tag)}
+  erb :post_successful
 end
